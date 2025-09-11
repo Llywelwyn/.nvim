70757360ee9e6e02d704641e9295051df13b0d7b
lew <lew@ily2.dev>
2025-09-11 11:51:29 +0100

    feat(ui): configured rose-pine

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -4,6 +4,24 @@ return {
         "rose-pine/neovim",
         name = "rose-pine",
         config = function()
+            require("rose-pine").setup({
+                variant = "auto",
+                dark_variant = "main",
+                dim_inative_windows = false,
+                extend_background_behind_borders = true,
+
+                enable = {
+                    terminal = true,
+                    legacy_highlights = true,
+                    migrations = true,
+                },
+
+                styles = {
+                    bold = true,
+                    italic = true,
+                    transparency = true,
+                },
+            })
             vim.cmd("colorscheme rose-pine")
         end,
     },
