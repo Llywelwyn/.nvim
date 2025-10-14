c0bd2daa7e177ae520a47989be47150753a5deb7
lew <lew@ily.rs>
2025-10-14 18:13:15 +0100

    feat(codecompanion): add copilot integration

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      -- https://codecompanion.olimorris.dev/extending/prompts
+    },
+  },
+}
